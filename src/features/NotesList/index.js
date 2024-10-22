cd0435a0d1853f5b966e32cd3da5212f37248f27
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-10-22 21:06:18 +0200

    NoteList updated

--- src/features/NotesList/index.js
+++ src/features/NotesList/index.js
@@ -2,15 +2,17 @@ import AddNote from "../AddNote";
 import Note from "../Note";
 import { NotesListContainer } from "./styled";
 
-const NotesList = ({ notes, handleAddNote }) => {
+const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
   return (
     <NotesListContainer>
       {notes.map((note) => (
         <Note
           key={note.id}
+          id={note.id}
           title={note.title}
           text={note.text}
           date={note.date}
+          handleDeleteNote={handleDeleteNote}
         />
       ))}
       <AddNote handleAddNote={handleAddNote} />
