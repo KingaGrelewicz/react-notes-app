e5c0cfb9a7b271a948ac3d7b9027a31d20fa9a43
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-11-30 16:06:44 +0100

    Provider and store added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Normalize } from 'styled-normalize';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Normalize } from "styled-normalize";
+import { Provider } from "react-redux";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Normalize />
-    <App />
+    <Provider store={store}>
+      <Normalize />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
