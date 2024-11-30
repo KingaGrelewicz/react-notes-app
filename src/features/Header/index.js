8e17bddb8c79487e9ef0fd658b97fe8bafd6b962
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-11-30 16:02:31 +0100

    Header component added to common file

--- src/features/Header/index.js
+++
@@ -1,7 +0,0 @@
-import { HeaderTitle } from "./styled";
-
-const Header = () => {
-  return <HeaderTitle>Notes</HeaderTitle>;
-};
-
-export default Header;
--- src/features/Header/styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const HeaderTitle = styled.h1`
-  font-size: 64px;
-  font-weight: 700;
-`;
