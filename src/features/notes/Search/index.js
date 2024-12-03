110ea2e0a11dd135a3c420db39a0ed56bf14c0d3
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-12-03 20:33:34 +0100

    Search updated

--- src/features/notes/Search/index.js
+++ src/features/notes/Search/index.js
@@ -1,34 +1,43 @@
 import { useMemo, useState } from "react";
+import { useSelector } from "react-redux";
 import { SearchContainer, SearchInput } from "./styled";
 import { IoMdSearch } from "react-icons/io";
 import NotesList from "../NotesList";
+import { selectNotes } from "../noteSlice";
 
-const Search = ({ notes }) => {
+const Search = () => {
+  const notes = useSelector(selectNotes);
   const [searchQuery, setSearchQuery] = useState("");
 
   const filteredNotes = useMemo(() => {
     if (!searchQuery.trim()) {
       return notes;
     }
+
     return notes.filter(
       (note) =>
-        note.text.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        note.title.toLowerCase().includes(searchQuery.toLowerCase())
+        note.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        note.text.toLowerCase().includes(searchQuery.toLowerCase())
     );
   }, [notes, searchQuery]);
 
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value.trim());
+  };
+
   return (
-    <>
+    <div>
       <SearchContainer>
         <IoMdSearch size="24px" />
         <SearchInput
-          onChange={(event) => setSearchQuery(event.target.value)}
+          onChange={handleSearchChange}
+          value={searchQuery}
           type="text"
-          placeholder="type to search..."
+          placeholder="Wpisz, aby wyszukać..."
         />
       </SearchContainer>
       <NotesList notes={filteredNotes} />
-    </>
+    </div>
   );
 };
 
