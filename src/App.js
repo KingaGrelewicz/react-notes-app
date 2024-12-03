fad32c425076d7b51f7b85791c6f39ab2c05b503
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-12-03 20:31:39 +0100

    App updated

--- src/App.js
+++ src/App.js
@@ -4,18 +4,14 @@ import { Globalstyle } from "./GlobalStyle";
 import { theme } from "./theme";
 import Search from "./features/notes/Search";
 import Header from "./common/Header";
-import { useSelector } from "react-redux";
-import { selectNotes } from "./features/notes/noteSlice";
 
 const App = () => {
-  const notes = useSelector(selectNotes);
-
   return (
     <ThemeProvider theme={theme}>
       <Container>
         <Globalstyle />
         <Header />
-        <Search notes={notes} />
+        <Search />
       </Container>
     </ThemeProvider>
   );
