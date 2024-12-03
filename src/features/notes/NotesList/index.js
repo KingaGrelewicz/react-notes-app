a52925fbc1fad8c5d413da3b8905a1dd815636a7
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-12-03 20:33:25 +0100

    noteList updated

--- src/features/notes/NotesList/index.js
+++ src/features/notes/NotesList/index.js
@@ -1,31 +1,49 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Note from "../Note";
 import AddNoteForm from "../AddNoteForm";
 import { NotesListContainer } from "./styled";
-import { deleteNote } from "../noteSlice";
+import { deleteNote, fetchNotesStart, selectNotesStatus } from "../noteSlice";
+import { useEffect } from "react";
 
 const NotesList = ({ notes }) => {
+  const status = useSelector(selectNotesStatus);
   const dispatch = useDispatch();
 
   const handleDeleteNote = (id) => {
     dispatch(deleteNote(id));
   };
 
-  return (
-    <NotesListContainer>
-      {notes.map((note) => (
-        <Note
-          key={note.id}
-          id={note.id}
-          title={note.title}
-          text={note.text}
-          date={note.date}
-          handleDeleteNote={handleDeleteNote}
-        />
-      ))}
-      <AddNoteForm />
-    </NotesListContainer>
-  );
+  useEffect(() => {
+    if (status === "initial") {
+      dispatch(fetchNotesStart());
+    }
+  }, [dispatch, status]);
+
+  if (status === "loading") {
+    return <p>uno momento</p>;
+  }
+
+  if (status === "error") {
+    return <p>ups!</p>;
+  }
+
+  if (status === "success" && notes?.length) {
+    return (
+      <NotesListContainer>
+        <AddNoteForm />
+        {notes.map((note) => (
+          <Note
+            key={note.id}
+            id={note.id}
+            title={note.title}
+            text={note.text}
+            date={note.date}
+            handleDeleteNote={handleDeleteNote}
+          />
+        ))}
+      </NotesListContainer>
+    );
+  }
 };
 
 export default NotesList;
