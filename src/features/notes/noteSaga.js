37bae76c178013831613ee431f7d9288655a442c
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-12-07 16:35:13 +0100

    noteSaga updated

--- src/features/notes/noteSaga.js
+++ src/features/notes/noteSaga.js
@@ -1,14 +1,19 @@
-import { call, delay, put, takeLatest } from "redux-saga/effects";
+import { call, put, select, takeLatest } from "redux-saga/effects";
 import {
+  addNote,
+  deleteNote,
   fetchNotesError,
   fetchNotesStart,
   fetchNotesSuccess,
+  selectNotes,
 } from "./noteSlice";
-import { getNotesFromLocalStorage } from "./notesLocalStorage";
+import {
+  getNotesFromLocalStorage,
+  saveNotesInLocalStorage,
+} from "./notesLocalStorage";
 
 function* fetchNotesHandler() {
   try {
-    yield delay(1000);
     const notes = yield call(getNotesFromLocalStorage);
     yield put(fetchNotesSuccess(notes));
   } catch (error) {
@@ -16,6 +21,11 @@ function* fetchNotesHandler() {
   }
 }
 
+function* saveNotesInLocalStorageHandler() {
+  const notes = yield select(selectNotes);
+  yield call(saveNotesInLocalStorage, notes);
+}
+
 export function* watchFetchNotes() {
   yield takeLatest(fetchNotesStart.type, fetchNotesHandler);
-}
+  yield takeLatest([addNote.type, deleteNote.type], saveNotesInLocalStorageHandler)}
