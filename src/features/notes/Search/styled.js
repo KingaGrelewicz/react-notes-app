6d4dfb35a4aef54f7c7069252623ff39915d8c9b
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-12-08 13:40:33 +0100

    component updated

--- src/features/notes/Search/styled.js
+++ src/features/notes/Search/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { IoMdSearch } from "react-icons/io";
 
 export const SearchContainer = styled.div`
   background: ${({ theme }) => theme.color.barColor};
@@ -8,10 +9,19 @@ export const SearchContainer = styled.div`
   display: grid;
   grid-template-columns: auto 1fr;
   grid-gap: 8px;
+  box-shadow: 3px 3px 10px 0px rgba(146, 148, 158, 0.5);
 `;
 
 export const SearchInput = styled.input`
   background: transparent;
   border: none;
   outline: none;
+  color: ${({ theme }) => theme.color.primaryTextColor};
+  &::placeholder {
+    color: ${({ theme }) => theme.color.primaryTextColor};
+  }
+`;
+
+export const SearchIcon = styled(IoMdSearch)`
+  color: ${({ theme }) => theme.color.iconsColor};
 `;
