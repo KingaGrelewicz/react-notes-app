bb5ecbdb54053191427e4311bf4fcfb919fd82f5
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-12-08 13:43:12 +0100

    theme splited between to modes

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,25 @@
-export const theme = {
+export const lightTheme = {
   color: {
-    primaryBackground: "#FEF68A",
-    addNoteBackground: "#67D7CC",
+    primaryBackground: "#FFFFFF",
+    noteBackground: "#6FD370",
+    addNoteBackground: "#6EC4BB",
     primaryTextColor: "#000000",
     buttonColor: "#FFFFFF",
+    buttonText: "#3E3A4A",
     barColor: "#F4F3F1",
+    iconsColor: "#000000",
+  },
+};
+
+export const darkTheme = {
+  color: {
+    primaryBackground: "#3E3A4A",
+    noteBackground: "#57CB58",
+    addNoteBackground: "#47D7C8",
+    primaryTextColor: "#FFFFFF",
+    buttonColor: "#FFFFFF",
+    buttonText: "#3E3A4A",
+    barColor: "#838281",
+    iconsColor: "#FFFFFF",
   },
 };
