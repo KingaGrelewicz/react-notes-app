bd36d73374e112ce73077224df00f878a8e07afc
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-12-08 13:46:03 +0100

    Logic of switching theme added to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { ThemeProvider } from "styled-components";
 import Container from "./common/Container";
 import { Globalstyle } from "./GlobalStyle";
-import { theme } from "./theme";
+import { darkTheme, lightTheme } from "./theme";
 import Search from "./features/notes/Search";
 import Header from "./common/Header";
+import { useSelector } from "react-redux";
+import { selectIsDarkTheme } from "./common/Theme/themeSlice";
 
 const App = () => {
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <Container>
         <Globalstyle />
         <Header />
